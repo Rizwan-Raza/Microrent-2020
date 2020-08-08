4883dc58c494495cfd40a9f28e6548a659933c41
Rex Terminous <rizwan.raza987@gmail.com>
2020-08-08 22:35:27 +0530

    Printer page added

--- about.php
+++ about.php
@@ -1,72 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
-<?php include "includes/head.html"; ?>
+  <?php include "includes/head.html"; ?>
 
-<title>About Inforent</title>
+  <title>About Inforent</title>
 
-<meta name="description"  content="Inforent Computers is one of the prominent rental companies in Delhi providing rental solutions. Our products and equipment are sophisticated and of superior." />
+  <meta name="description" content="Inforent Computers is one of the prominent rental companies in Delhi providing rental solutions. Our products and equipment are sophisticated and of superior." />
 
-<!-- <link rel="canonical" href="http://inforentcomputers.com/about-us/" /> -->
+  <!-- <link rel="canonical" href="http://inforentcomputers.com/about-us/" /> -->
 
 </head>
+
 <body>
 
-<div class="container"> 
-  <!--center shadow starts-->
-  <section class="center-shadow"> 
-    <!--header starts-->
-    <header>
-    <?php include "includes/nav.html"; ?>
-
-    <?php include "includes/inner-banner.html"; ?>
+  <div class="container">
+    <!--center shadow starts-->
+    <section class="center-shadow">
+      <!--header starts-->
+      <header>
+        <?php include "includes/nav.html"; ?>
 
 
-</header>
+      </header>
 
-<!--header ends-->
+      <!--header ends-->
 
-<section id="body-container">
-  <div class="row">
-    <div class="col-md-12"> 
-      
-      <!--content starts-->
-      
-      <section class="content-container inner-content">
-        <div class="row">
-          <div class="col-md-9">
-            <!--h1>
-                          </h1-->
-            <h1>About Us</h1>
-<p style="text-align: justify;">Info Rent is one of the prominent <a href="http://inforentcomputers.com/">computer rental companies in Delhi</a> providing rental solutions.<br />
-Our products and equipment are sophisticated and of superior quality. We are backed by a vast industry experience and have the expertise in dealing with computers, laptops, mobile phones, multimedia projection equipment, office equipment and more. We provide our equipment for rent in Delhi and offer required installation services as well.</p>
-<p style="text-align: justify;">Combining the highest level of service with the most cutting-edge technology, the Inforent Computers is ready to help you. We can fulfill your short term office equipment needs, as well as provide the AV equipment rentals and technology solutions for professional meetings and events. Whether it&#8217;s specialty event equipment such as LED and video walls, or general business technology such as high-end desktops, laptops, and servers, we have it all.</p>
-<p style="text-align: justify;">We are known for our quality and customer satisfaction.</p>
-<p style="text-align: justify;">We provide computers, laptops, plasma TVs, projectors, audio visual equipment, Office Equipment, Security Equipment, Mobile Phones, Software, Apple MAC, UPS etc. on rental basis. We cater to the needs of corporate events, seminars, conferences, official meetings etc. in Delhi.</p>
-<p style="text-align: justify;">Our existing customers repeat business and continue to avail our services. Our quality products, latest brands and well-managed inventory provide highest level of customer satisfaction.</p>
-            			
-        <?php include "includes/contact.html";?>
-          </div>
-          <div class="col-md-3">
-            <?php include "includes/sidebar.html"; ?>
-          </div>
-        </div>
+      <section id="body-container">
+
+        <section class="content-container">
+          <h1><span>About Us</span></h1>
+          <p>Microrent India was established in 1989 and is one of the pioneers in computer renting. With 30 year plus experience we have a strong customer base of more than 1500 plus companies in Delhi and NCR. Our quality service products has made us the lasrgest rental company.</p>
+
+          <p>We have a team of qualified engineers to give expertised service for our products. Our product range includes Desktop / Laptops / Servers / Workstations / Printers / Projectors / Networking Products / Bar code scanners / iPads / Scanners / Audio Visual equipments for office use, trainings, conference, examinations / BPO service etc.</p>
+
+          <p>Microrent India is also into sales of IT Products which include Desktop / Servers / Laptops / Printers / Projector / Networking solutions etc. We are brand partners to Dell, Apple, HP, Lenovo. We also sell customized computers and servers. We also have leasing option.
+          </p>
+          <?php include "includes/partners.html"; ?>
+          <?php include "includes/clients.html"; ?>
+          <?php include "includes/contact.html"; ?>
+        </section>
+
       </section>
-      
-      <?php include "includes/partners.html"; ?>
-      
-    </div>
+    </section>
+
+    <!--center shadow ends-->
+
+    <?php include "includes/footer.html"; ?>
+
   </div>
-</section>
-</section>
-
-<!--center shadow ends--> 
-
-<?php include "includes/footer.html"; ?>
-	  
-</div>
-<?php include "includes/bottom.html"; ?>
+  <?php include "includes/bottom.html"; ?>
 </body>
-</html>
 
+</html>
