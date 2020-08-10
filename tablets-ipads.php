7177882f366403ea04eb82609464b12c7c22234b
Rex Terminous <rizwan.raza987@gmail.com>
2020-08-10 22:29:49 +0530

    iPads added

--- tablets-ipads.php
+++ tablets-ipads.php
@@ -1,96 +1,144 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
-<?php include "includes/head.html"; ?>
+  <?php include "includes/head.html"; ?>
 
-<title>Microrent India</title>
-<meta name="description"  content="Inforent offers Tablets on Rent in Delhi, Noida and Gurgaon at competitve prices. Call now to rent a tab." />
+  <title>Microrent India</title>
+  <meta name="description" content="Inforent offers Tablets on Rent in Delhi, Noida and Gurgaon at competitve prices. Call now to rent a tab." />
 
-<!-- <link rel="canonical" href="http://inforentcomputers.com/tablets-on-rent-in-delhi/" /> -->
+  <!-- <link rel="canonical" href="http://inforentcomputers.com/tablets-on-rent-in-delhi/" /> -->
+  <link href="css/laptop.css" rel="stylesheet">
+  <style>
+    .prods .panel {
+      padding: 1em;
+      border-color: #ddd;
+      text-align: center;
+    }
+
+    .prods .panel img {
+      width: 50%;
+      margin: 2em;
+    }
+  </style>
+
 <body>
+  <div class="container">
+    <!--center shadow starts-->
+    <section class="center-shadow">
+      <!--header starts-->
+      <header>
+        <?php include "includes/nav.html"; ?>
 
-<div class="container"> 
-  <!--center shadow starts-->
-  <section class="center-shadow"> 
-    <!--header starts-->
-    <header>
-    <?php include "includes/nav.html"; ?>
-    <?php include "includes/inner-banner.html"; ?>
+      </header>
 
+      <!--header ends-->
 
-</header>
-
-<!--header ends-->
-
-<section id="body-container">
-  <div class="row">
-    <div class="col-md-12"> 
-      
-      <!--content starts-->
-      
-      <section class="content-container inner-content">
+      <section id="body-container">
         <div class="row">
-          <div class="col-md-9">
-          <h1>
-              Tablets on Rent in Delhi &#038; NCR            </h1>
-<div style="display:block; overflow:hidden;" class="product-row">
-            <div class="photo-box"><a href="http://inforentcomputers.com/contact-us/">
-                <img width="225" alt="" src="http://inforentcomputers.com/wp-content/uploads/2019/10/Galaxy-Tab-S5e-LTE-1.jpg"></a> 
-			</div>
-          <div class="details">
-			<h3><strong>Galaxy Tab S5e (LTE)</strong></h3>
-<p>CPU SPEED 2GHZ, 1.7GHZ/ (MAIN DISPLAY) 26.72CM  (10.5&quot;)/ VIDEO RECORDING RESOLUTION UHD  4K (3840 X 2160)@30FPS / RAM_S(4  GB) STORAGE (GB)64 / USB Version USB 3.1 Gen 1</p>
-            
-<p>Super Thin - 5.5mm<br>
-Dex Mode and Keyboard Support<br>
-Call-Message Continuity</p>
+          <div class="col-md-12">
 
-  </div>
-        </div>
-            		<div style="display:block; overflow:hidden;" class="product-row">
-            <div class="photo-box"><a href="http://inforentcomputers.com/contact-us/">
-                <img width="225" height="161" alt="" src="http://inforentcomputers.com/wp-content/uploads/2016/07/tablets.jpg"></a> 
-			</div>
-          <div class="details">
-			<h3></h3>
-			<h3>SAMSUNG TAB SM-T805</h3>
-<p><strong>10.5&#8243;SCREEN / INTERNAL MEMORY</strong> 16 GB, 3 GB RAM</p>
-<p>&nbsp;</p>
-<h3>SAMSUNG GALAXY TAB E</h3>
-<p><strong>1.3 GHZ /9.6” SCREEN/ INTERNAL MEMORY 8GB/ 1.5 GB RAM</strong></p>
-<p>&nbsp;</p>
-<h3 style="color: #00cbd0;">SAMSUNG GALAXY J MAX (SM-T285YZD)</h3>
-<p>1.5 GHZ / Android v5.1 (Lollipop) OS/ 1.5 GB RAM /<br />
-Storage 8 GB / 8 MP Primary Camera / 2 MP Secondary Camera /7 inch Screen / Supports 4G LTE Network /Dual SIM/ Resolution 800 x 1280 pixels.</p>
-<h3 style="color: #00cbd0;">IBALL GORGEO 4GL (4G + Wifi, Voice Calling)</h3>
-<p>Dual Sim, GSM + LTE / Android 5.1 (Lollipop) / Quad Core /<br />
-IPS Display / 5 MP Rear Camera / 2 MP Front Camera / 7 inch Screen / 1 GB RAM / 8 GB Storage / 3500 mAh Battery.</p>
-<p><a href="http://inforentcomputers.com/wp-content/uploads/2016/12/Iball-Gorgeo1-1.jpg"><img class="alignnone wp-image-546 size-full" src="http://inforentcomputers.com/wp-content/uploads/2016/12/Iball-Gorgeo1-1.jpg" alt="" width="500" height="237" srcset="http://inforentcomputers.com/wp-content/uploads/2016/12/Iball-Gorgeo1-1.jpg 500w, http://inforentcomputers.com/wp-content/uploads/2016/12/Iball-Gorgeo1-1-300x142.jpg 300w" sizes="(max-width: 500px) 100vw, 500px" /></a></p>
-            <p><a class="btn" href="http://inforentcomputers.com/contact-us/">Enquire Now</a></p>
+            <!--content starts-->
+
+            <section class="content-container">
+              <div class="row">
+                <div class="col-sm-12 col-md-9">
+                  <h1><span>iPad on Rent in Delhi, Noida and Gurgaon</span></h1>
+                  <br />
+
+                  <div class="row prods">
+                    <div class="col-sm-12 col-md-6">
+                      <div class="panel">
+                        <img src="img/laptop" alt="Laptop">
+                        <p>Apple iPad Pro 2020, 32.76 cm (12.9 ich) Wi-Fi + Cellular Tablet 256 GB, Space Grey. 12 MP Wide &amp; 10 MP Ultra Wide Primary Camera</p>
+                      </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                      <div class="panel">
+                        <img src="img/laptop" alt="Laptop">
+                        <p>Apple iPad Pro (12.9 inch, Wi-Fi 64 GB), Space Grey (3rd Generation)</p>
+                      </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                      <div class="panel">
+                        <img src="img/laptop" alt="Laptop">
+                        <p>Apple Portable Tablet / PC, Wi-Fi Model, A10X Fusion Processor, 4GB RAM, 5x Digital Zoom Camera</p>
+                      </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                      <div class="panel">
+                        <img src="img/laptop" alt="Laptop">
+                        <p>Apple iPad 2020, 32.76cm (12.9 inch) Wi-Fi + Cellular Tablet 1TB, Space Grey</p>
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <h1><span>Other models available: </span></h1>
+                  <br />
+                  <p>✔️ iPad Mini -16 GB, Wi-Fi</p>
+                  <p>✔️ Apple iPad Pro, 10.5 inch (64 GB, Wi-Fi, Rose Gold)</p>
+                  <p>✔️ Lenovo Yoga Tablet 3 Pro</p>
+                  <p>✔️ Samsung Galaxy Tab T211</p>
+                  <p>✔️ Apple iPad Pro 2018, 64 GB / 12.9 inch / With Wi-Fi / Space Grey</p>
+                  <p>✔️ Apple iPad Air 2019 - 26.67 cm (10.5 inch) Wi-Fi + Cellular Tablet, 64 GB, Grey</p>
+                  <p>✔️ Apple iPad Pro (11-inch, Wi-Fi, 64 GB), Silver</p>
+                  <p>✔️ Apple iPad Pro (4th Generation, A12Z, 64 Bit Architecture)</p>
+                  <p>✔️ Many more models to meet your requirements.</p>
+                </div>
+                <div class="col-sm-12 col-md-3 contact" style="border-left: 1px solid #ddd;">
+                  <h1 class="default">Quick Query</h1>
+                  <?php include "includes/query.html"; ?>
+                </div>
               </div>
-        </div>
-        <?php include "includes/contact.html";?>
-          </div>
-          <div class="col-md-3">
-            <?php include "includes/sidebar.html"; ?>
+              <div class="partners-section">
+                <h1><span>Our Brand Partners</span></h1>
+                <div id="partnersSliderHome" class="owl-carousel owl-theme">
+                  <div class="item">
+                    <div class="box"> <img class="lazyOwl" data-src="img/client-logo8.png" alt="Apple"> </div>
+                  </div>
+                  <div class="item">
+                    <div class="box"> <img class="lazyOwl" data-src="img/client-logo10.png" alt="Lenovo on Rent"> </div>
+                  </div>
+                  <div class="item">
+                    <div class="box"> <img class="lazyOwl" data-src="img/client-logo7.png" alt="Samsung Computers on Rent">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <br />
+              <br />
+              <section class="specials">
+                <div class="row">
+                  <div class="col-sm-12 col-md-4">
+                    <img src="img/experience.png" />
+                    <label>30 Years of experience</label>
+                  </div>
+                  <div class="col-sm-12 col-md-4">
+                    <img src="img/uptime.png" />
+                    <label>98% Uptime</label>
+                  </div>
+                  <div class="col-sm-12 col-md-4">
+                    <img src="img/credibility.png" />
+                    <label>Credibility Serving Leading Brands</label>
+                  </div>
+                </div>
+              </section>
+              <?php include "includes/contact.html"; ?>
+            </section>
+
           </div>
         </div>
       </section>
-      
-      <?php include "includes/partners.html"; ?>
-      
-    </div>
+    </section>
+
+    <!--center shadow ends-->
+
+    <?php include "includes/footer.html"; ?>
+
   </div>
-</section>
-</section>
-
-<!--center shadow ends--> 
-
-<?php include "includes/footer.html"; ?>
-	  
-</div>
-<?php include "includes/bottom.html"; ?>
+  <?php include "includes/bottom.html"; ?>
 </body>
-</html>
 
+</html>
